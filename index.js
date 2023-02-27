f56e1aae8b6ba42cb23d83d650b1af3c38877d50
Bailey Seely <65569673+baileyseely1@users.noreply.github.com>
2023-02-27 15:03:00 -0500

    update

--- index.js
+++ index.js
@@ -5,23 +5,34 @@ const options = {
   },
 };
 
+let dataArr = [];
+
 const initColor = document.getElementById("init-color");
 const mode = document.getElementById("mode");
 
 document.getElementById("get-color").addEventListener("click", (e) => {
   e.preventDefault();
-  renderColors();
-});
 
-const renderColors = () => {
   fetch(
     `https://www.thecolorapi.com/scheme?hex=${initColor.value.substring(
       1
     )}&count=5&mode=${mode.value}`
   )
     .then((res) => res.json())
-    .then((data) => console.log(data));
-  // .then(data => data.map(color => {
-  //     document.getElementById('color-container')
-  // }))
+    .then((data) => {
+      dataArr = data.colors;
+    });
+  renderColors();
+});
+
+const renderColors = () => {
+  const dataHtml = dataArr
+    .map((color) => {
+      return `<div class="color" id="${color.name.value}">
+      <img class="color-img" src="${color.image.named}"></img>
+      <p>${color.hex.value}</p>
+      </div>`;
+    })
+    .join();
+  document.getElementById("color-container").innerHTML = dataHtml;
 };
