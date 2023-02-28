09bcc4aa25a849d1be5e0e548f49558846a11904
Bailey Seely <65569673+baileyseely1@users.noreply.github.com>
2023-02-27 21:48:58 -0500

    fixed bug on random color
    fixed bug where the copy to clipboard and html weren't displaying on random colors -> not very DRY need to refactor later probably

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import { generateRandomColors } from "/utils.js";
 
+let dataArr = [];
+let randomColor = false;
+const initColor = document.getElementById("init-color");
+const node = document.createElement("div");
+let textnode = document.createElement("p");
+const mode = document.getElementById("mode");
 const options = {
   method: "GET",
   headers: {
@@ -7,27 +13,32 @@ const options = {
   },
 };
 
-let dataArr = [];
-let randomColor = false;
-const initColor = document.getElementById("init-color");
-const mode = document.getElementById("mode");
+document.getElementById("get-color").addEventListener("click", (e) => {
+  e.preventDefault();
+  renderColors();
+  randomColor = false;
+});
+
+document.getElementById("get-random").addEventListener("click", () => {
+  randomColor = true;
+  renderColors();
+});
 
 function handleCopyToClip() {
   dataArr.forEach((element) => {
     const elNameValue = document.getElementById(element.name.value);
     elNameValue.addEventListener("click", () => {
       if (!document.getElementById("copy-div")) {
-        const node = document.createElement("div");
         node.setAttribute("id", "copy-div");
-        const textnode = document.createElement("p");
+
         textnode.textContent = `🪄 copied ${element.hex.value} to clipboard `;
         node.appendChild(textnode);
         elNameValue.appendChild(node);
+        navigator.clipboard.writeText(element.hex.value);
         setTimeout(() => {
           elNameValue.removeChild(node);
         }, 2000);
       }
-      navigator.clipboard.writeText(element.hex.value);
     });
     elNameValue.style.backgroundColor = element.hex.value;
     document.getElementById(element.hex.value).textContent = element.hex.value;
@@ -51,28 +62,32 @@ const renderColors = () => {
         })
         .join("");
 
-      handleCopyToClip();
-
       if (randomColor) {
         dataArr.forEach((color) => {
+          const elNameVal = document.getElementById(color.name.value);
+          elNameVal.addEventListener("click", () => {
+            if (!document.getElementById("copy-div")) {
+              navigator.clipboard.writeText("#" + newColor);
+              const node = document.createElement("div");
+              node.setAttribute("id", "copy-div");
+              const textnode = document.createElement("p");
+              textnode.textContent = `🪄 copied ${newColor} to clipboard `;
+              node.appendChild(textnode);
+              elNameVal.appendChild(node);
+              setTimeout(() => {
+                elNameVal.removeChild(node);
+              }, 2000);
+            }
+          });
           const newColor = generateRandomColors(1);
           document.getElementById(color.name.value).style.backgroundColor =
             "#" + newColor;
           document.getElementById(color.hex.value).textContent = "#" + newColor;
         });
+      } else {
+        handleCopyToClip();
       }
     });
 };
-
-document.getElementById("get-color").addEventListener("click", (e) => {
-  e.preventDefault();
-  renderColors();
-  randomColor = false;
-});
-
-document.getElementById("get-random").addEventListener("click", () => {
-  randomColor = true;
-  renderColors();
-});
 
 renderColors();
